8cd43b7560e1afa21a5c20417723712ec838f0d7
QNH-NET\abdullah.kucuk <akucuk87@hotmail.com>
2016-10-16 07:58:35 +0200

    Intents file added

---
+++ app/src/main/java/com/example/abdullahkucuk/fruittuin/Constants/Intents.java
@@ -0,0 +1,9 @@
+package com.example.abdullahkucuk.fruittuin.Constants;
+
+/**
+ * Created by abdullah.kucuk on 16-10-2016.
+ */
+public class Intents {
+    final public static String YES_INTENT = "YesIntent";
+    final public static String NO_INTENT = "NoIntent";
+}
