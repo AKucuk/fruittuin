c1f0ae9ccead16ce240f6a032cd877587f9a9bb8
Pieter Plomp <pplomp84@live.nl>
2016-10-30 14:00:39 +0100

    Navigation drawer menu added

---
+++ app/src/main/java/com/example/abdullahkucuk/fruittuin/Fragments/SpeurtochtPart1Fragment.java
@@ -0,0 +1,32 @@
+package com.example.abdullahkucuk.fruittuin.Fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.abdullahkucuk.fruittuin.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SpeurtochtPart1Fragment extends Fragment {
+
+
+    public SpeurtochtPart1Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_speurtocht_part1, container, false);
+    }
+
+}
+
+
