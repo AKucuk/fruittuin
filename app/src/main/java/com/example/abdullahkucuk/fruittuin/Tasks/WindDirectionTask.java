d128a8a6b80f3062480e0b41a4ca394f0e60c00d
Abdullah Kucuk <a.kucuk@leaseproces.nl>
2017-01-24 19:24:40 +0100

    Fix errors

--- app/src/main/java/com/example/abdullahkucuk/fruittuin/Tasks/WindDirectionTask.java
+++ app/src/main/java/com/example/abdullahkucuk/fruittuin/Tasks/WindDirectionTask.java
@@ -62,17 +62,14 @@ public class WindDirectionTask extends AsyncTask<String, Void, WindDirection> {
         }
 
         Weather weather = new Weather(promptWindDirectionFragment.getContext(), "Amsterdam");
-        switch(windDirection) {
-            case weather.getRoughWindDirection():
-                showImageQuiz();
-                break;
-            default:
-                Toast.makeText(promptWindDirectionFragment.getContext(), "De wind komt van een andere kant, probeer opnieuw!", Toast.LENGTH_LONG)
-                        .show();
-                return;
+        if(windDirection == weather.getRoughWindDirection()) {
+            showImageQuiz();
+        }
+        else {
+            Toast.makeText(promptWindDirectionFragment.getContext(), "De wind komt van een andere kant, probeer opnieuw!", Toast.LENGTH_LONG)
+                    .show();
         }
 
-    //TODO: Koppel realtime info van Weather API
 
         /*LuisEntityModel entity = entities.get(0);
 
